27837ff679b0f00cf894454c13d902c975286989
Erin Winking <erin@douglasavenue.com>
2022-05-17 16:28:04 -0500

    Set up back end, connected react app to django app.

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,8 +1,19 @@
+import { useState, useEffect } from 'react'
 import { Row, Col } from 'react-bootstrap'
+import axios from 'axios'
 import Product from '../components/Product'
-import products from '../products'
 
 function HomeScreen() {
+	const [products, setProducts] = useState([])
+
+	useEffect(() => {
+		const fetchProducts = async () => {
+			const { data } = await axios.get('/api/products/')
+			setProducts(data)
+		}
+
+		fetchProducts()
+	}, [])
 	return (
 		<div>
 			<h2>Latest Products</h2>
--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,11 +1,22 @@
+import { useState, useEffect } from 'react'
 import { Link, useParams } from 'react-router-dom'
+import axios from 'axios'
 import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
 
 function ProductScreen() {
 	const params = useParams()
-	const product = products.find((p) => p._id === params.id)
+
+	const [product, setProduct] = useState([])
+
+	useEffect(() => {
+		const fetchProduct = async () => {
+			const { data } = await axios.get(`/api/products/${params.id}`)
+			setProduct(data)
+		}
+
+		fetchProduct()
+	}, [params.id])
 
 	return (
 		<div>
